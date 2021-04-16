6d7b0adade574230bd4a3ecd51abaa5ef9a85125
Andrea Vidal <11.andrea.v.p@gmail.com>
2021-04-16 10:33:06 +0200

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+
+  return (
+    <nav className="navegacion">
+      <ul>
+        <li><a href=""><img src="" alt="logo Club" /></a></li>
+        <li><a href="">Noticias</a></li>
+        <li><a href="">Equipos</a></li>
+        {/* <li><a href="">Contacta</a></li> */}
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
