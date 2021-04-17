d55387b6b997878c6eb5f25f6fee79ad09a9caaf
Andrea Vidal <11.andrea.v.p@gmail.com>
2021-04-17 20:57:26 +0200

    Pagina noticias

--- src/paginas/Principal.js
+++ src/paginas/Principal.js
@@ -1,7 +1,22 @@
-const Principal = () => {
+import '../estilos/noticias.css';
+import { useContext } from "react";
+import NoticiasContext from "../context/NoticiasContext";
 
+const Principal = () => {
+  const { datosNoticias } = useContext(NoticiasContext);
   return (
-    <></>
+    <div className="contenido">
+      <h1 className="noticias">Noticias</h1>
+      {datosNoticias.map(noticia => (
+        <article id={noticia["_id"]["$oid"]}>
+          <h2>{noticia.titulo}</h2>
+          {/* <img className="imagen" src={noticia.img.link} alt={noticia.img.Alt} /> */}
+          <p>{noticia.texto}</p>
+          <button className="mas">Leer más...</button>
+        </article>
+      ))}
+
+    </div>
   );
 }
 
