dcfd5728929aeb9b1e056fb69c9e4c3b3545c1eb
Andrea Vidal <11.andrea.v.p@gmail.com>
2021-04-18 13:25:22 +0200

    Pagina noticia/id

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ const Footer = () => {
   return (
     <footer className="pie">
       <div className="redes">
-        <a href="https://www.facebook.com/" target="_blank" rel="noreferrer"><img className="iconos" src="./iconos/facebook_icon.png" alt="" /></a>
-        <a href="https://www.instagram.com/" target="_blank" rel="noreferrer"><img className="iconos" src="./iconos/instagram_icon.png" alt="" /></a>
-        <a href="https://www.twitter.com/" target="_blank" rel="noreferrer"><img className="iconos" src="./iconos/twitter_icon.png" alt="" /></a>
+        <a href="https://www.facebook.com/" target="_blank" rel="noreferrer"><img className="iconos" src="../iconos/facebook_icon.png" alt="" /></a>
+        <a href="https://www.instagram.com/" target="_blank" rel="noreferrer"><img className="iconos" src="../iconos/instagram_icon.png" alt="" /></a>
+        <a href="https://www.twitter.com/" target="_blank" rel="noreferrer"><img className="iconos" src="../iconos/twitter_icon.png" alt="" /></a>
       </div>
     </footer>
   );
--- src/paginas/Principal.js
+++ src/paginas/Principal.js
@@ -1,22 +1,30 @@
 import '../estilos/noticias.css';
 import { useContext } from "react";
+import { useHistory } from 'react-router-dom';
 import NoticiasContext from "../context/NoticiasContext";
 
 const Principal = () => {
+  let history = useHistory();
   const { datosNoticias } = useContext(NoticiasContext);
+  const clickNoticia = e => {
+    const path = `/noticia/${e.target.id}`;
+    history.push(path);
+  };
+
   return (
     <div className="contenido">
       <h1 className="noticias">Noticias</h1>
       {datosNoticias.map(noticia => (
-        <article id={noticia["_id"]["$oid"]}>
+        <article key={noticia["_id"]["$oid"]}>
           <h2>{noticia.titulo}</h2>
           {/* <img className="imagen" src={noticia.img.link} alt={noticia.img.Alt} /> */}
           <p>{noticia.texto}</p>
-          <button className="mas">Leer más...</button>
+          <span className="mas" id={noticia["_id"]["$oid"]} onClick={clickNoticia}>Leer más...</span>
         </article>
-      ))}
+      ))
+      }
 
-    </div>
+    </div >
   );
 }
 
