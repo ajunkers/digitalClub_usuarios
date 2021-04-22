76faab54827de8f9bb9acfc0dfaa0f88b09a207d
Andrea Vidal <11.andrea.v.p@gmail.com>
2021-04-22 12:16:25 +0200

    pagina de equipos

---
+++ src/context/EquipoContext.js
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+const EquipoContext = createContext();
+EquipoContext.displayName = "EquipoContext";
+
+export default EquipoContext;
--- src/paginas/Equipo.js
+++ src/paginas/Equipo.js
@@ -1,6 +1,10 @@
 import { useEffect } from "react";
 import { useParams } from "react-router";
 import useFetch from "../hooks/useFetch";
+import '../estilos/equipos.css';
+import EquipoContext from "../context/EquipoContext";
+import ListaStaff from "../components/ListaStaff";
+import ListaJugadores from "../components/ListaJugadores";
 
 const Equipo = () => {
   const { id } = useParams();
@@ -12,6 +16,25 @@ const Equipo = () => {
   }, [url, pedirEquipo]);
 
 
-  return (<h1>Equipo</h1>);
+  return (
+    <>
+      < EquipoContext.Provider value={{ equipo }}>
+        {equipo ?
+          <article>
+            <h1 className="equipo">{equipo.nombre}</h1>
+            <section className="columnas colStaff">
+              <div className="columna">
+                <img className="imgEquipo" src={equipo.img ? equipo.img.link : "../placeholder.png"} alt={equipo.img ? equipo.img.alt : "imagen vacia"} />
+                <ListaStaff />
+              </div>
+              <div className="columna colJugadores">
+                <ListaJugadores />
+              </div>
+            </section>
+          </article>
+          : ""}
+      </EquipoContext.Provider>
+    </>
+  );
 }
 export default Equipo;
