48787fa47947fb4807f748ece815e1f104ce5562
Andrea Vidal <11.andrea.v.p@gmail.com>
2021-04-26 22:17:56 +0200

    feature/espacios en las noticias

--- src/components/DatosNoticias.js
+++ src/components/DatosNoticias.js
@@ -13,12 +13,16 @@ const DatosNoticia = () => {
 
   const fecha = datosNoticia ? datosNoticia["updated_at"].split("T") : <></>;
 
+  const arrayTexto = datosNoticia ? datosNoticia.texto.split("\n").filter(parrafo => parrafo !== "") : "";
+
   return (
     datosNoticia ?
       <>
         <h1 className="noticias">{datosNoticia.titulo}</h1>
         <img src={datosNoticia.img ? datosNoticia.img.link : ""} alt={datosNoticia.img ? datosNoticia.img.alt : ""} className="imagenNoticia" />
-        <p className="texto">{datosNoticia.texto ? datosNoticia.texto : ""}</p>
+        {arrayTexto.map(parrafo => (
+          <p className="texto">{parrafo}</p>
+        ))}
         <span className="actualizado">Última actualización: {fecha[0]}</span>
       </>
       : <></>
